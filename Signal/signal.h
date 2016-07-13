bdf5a853255e6d842b911784ff747586a950cd64
anon <gitdev@web.de>
2016-07-13 19:34:39 +0200

    documentation documentation documentation

--- Signal/signal.h
+++ Signal/signal.h
@@ -24,8 +24,43 @@ using namespace std;
 enum ModificationType {ADD, SUBTRACT, MULTIPLY, DIVIDE, ADD_SIGNAL, SUBTRACT_SIGNAL, MULTIPLY_SIGNAL, DIVIDE_SIGNAL};
 
 /**
- * class signal
- * @brief class inherited from std::vector which is for basic calculations on
+ * class Signal
+ * @brief class inherited from std::vector which is for basic calculations on a array of int values
+ * The class Signal can make basic calculations of like :
+ *  --> addition         of scalar values
+ *  --> substraction     of scalar values
+ *  --> multiplication with scalar values
+ *  --> division         by scalar values
+ *
+ *  --> addition         of two Signals
+ *  --> substraction     of two Signals
+ *  --> multiplication with two Signals
+ *  --> division       with two Signals
+ *
+ *  --> TODO
+ *
+ * The class Signal supports data with multiple channels!
+ * For example if one has stored data with different content in the same Signal,
+ * (like in pictures for every pixel one value for red, one for green, one for blue
+ * and one for alpha-channel (transparency),) the Signal class can work on either
+ *
+ *  --> all channels (R,G,B,A) at once   --> useMultiChannel == false
+ *  --> or on only one channel per time  --> useMultiChannel == true
+ *
+ * The multi-channel-mode can be activated by setting first the number of channels
+ *  --> signal_object.setChannelsCount(4);
+ *
+ * and then setting the useMultiChannel-flag
+ *  --> signal_object.setUseMultiChannel(true);
+ *
+ * To define on which channel the Signal class has to work, set the selectedChannel - attribute
+ * Please note that channels are indexed beginning at index 0 to index channelsCount - 1
+ *  --> signal_object.setSelectedChannel(2)
+ *
+ * If one has done these three steps, all mathematical operators and the functions
+ *   - modifySignal
+ *   - analyseSignal
+ * work on only the data of the selected channel
  *
  */
 class Signal : public vector<int>{
@@ -37,6 +72,7 @@ class Signal : public vector<int>{
                int  channelsCount_    =       1,   int  selectedChannel_  =       0,
                bool useCutOffToRange_ =   false,   int  minValue_         = INT_MIN,
                int  maxValue_         = INT_MAX                                   );
+
         // copy - constructor
         Signal(const Signal& other_, bool copyVectorData_ = true); // copy-constructor
 
@@ -86,6 +122,3 @@ class Signal : public vector<int>{
 };
 
 #endif // SIGNAL_H
-
-'
-4'
