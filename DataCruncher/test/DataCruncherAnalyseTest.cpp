99934d4ee04b728e1cd05a33ea6ac54cb04cf7ce
anon <gitdev@web.de>
2017-10-22 23:06:57 +0200

    added testcases for DataCruncher.calcAverage()

--- DataCruncher/test/DataCruncherAnalyseTest.cpp
+++ DataCruncher/test/DataCruncherAnalyseTest.cpp
@@ -14,8 +14,21 @@
 
 TEST_CASE("Test DataCruncher.calcAverage") {
     DataCruncher DC;
+
     SECTION("calcAverage with integers") {
         DC.vData = {3,6,-3,5,7,2,1,6};
         REQUIRE (DC.calcAverage() == 3.375);
+
+        DC.vData = {10000,234235,290294098,2340209,23232346574};
+        REQUIRE (DC.calcAverage() == 4705045023.2);
+
+        DC.vData = {-23425234,-7696858,-84863637,-82828374,-4738384};
+        REQUIRE (DC.calcAverage() == -40710497.4);
+
+        DC.vData = {0,0,0,0,0,0,0,0,0,0,0,0,0};
+        REQUIRE (DC.calcAverage() == 0);
+
     }
+
+
 }
