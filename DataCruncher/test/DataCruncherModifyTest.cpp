a2028ff037ed69849ee9483050e29447a8f79b64
anon <gitdev@web.de>
2018-01-04 22:02:48 +0100

    fixed bugs in testcases and added more testcases for z-transform

--- DataCruncher/test/DataCruncherModifyTest.cpp
+++ DataCruncher/test/DataCruncherModifyTest.cpp
@@ -13,32 +13,74 @@
 #include "DataCruncher.h"
 
 TEST_CASE("Test DataCruncher.zTransform") {
-    DataCruncher DC;
-    SECTION("test") {
-        DC.vData = {3,6,-3,5,7,2,1,6};
-    }
-    SECTION("test") {
-        DC.vData = {6,6};
+    const double TOLERANCE = 0.01;
+
+    DataCruncher DCBefore, DCAfter;
+
+    SECTION("zTransform with integers") {
+        SECTION("zTransform with small unsigned integers") {
+            DCBefore.vData = {3,6,3,5,7,2,1,6};
+        }
+
+        SECTION("zTransform with small signed integers") {
+            DCBefore.vData = {-3,-6,-3,-5,-7,-2,-1,-6};
+        }
+
+        SECTION("zTransform with small mixed-signed integers") {
+            DCBefore.vData = {3,6,-3,5,7,2,-1,-6};
+        }
+
+        SECTION("zTransform with big unsigned integers") {
+            DCBefore.vData = {23425234,7696858,84863637,82828374,4738384};
+        }
+
+        SECTION("zTransform with big signed integers") {
+            DCBefore.vData = {-23425234,-7696858,-84863637,-82828374,-4738384};
+        }
+
+        SECTION("zTransform with big mixed-signed integers") {
+            DCBefore.vData = {-23425234,7696858,84863637,-82828374,-4738384};
+        }
     }
 
-        double averageBeforeZ  = DC.calcAverage();
-        double varianceBeforeZ = DC.calcVariance();
-        DC.zTransform(true);
-        double averageAfterZ  = DC.calcAverage();
-        double varianceAfterZ = DC.calcVariance();
-        REQUIRE(averageAfterZ == 0);
-        REQUIRE(varianceAfterZ == 1);
-        DC.undoZTransform(averageBeforeZ,varianceAfterZ,true);
-        REQUIRE(averageAfterZ == averageBeforeZ);
-        REQUIRE(varianceAfterZ == varianceBeforeZ);
-        REQUIRE(DC.vData[0] ==  3);
-        REQUIRE(DC.vData[0] ==  6);
-        REQUIRE(DC.vData[0] == -3);
-        REQUIRE(DC.vData[0] ==  5);
-        REQUIRE(DC.vData[0] ==  7);
-        REQUIRE(DC.vData[0] ==  2);
-        REQUIRE(DC.vData[0] ==  1);
-        REQUIRE(DC.vData[0] ==  6);
+    SECTION("zTransform with doubles") {
+        SECTION("zTransform with small unsigned doubles") {
+            DCBefore.vData = {3.123,6.0,3.1,5.1234,7.3,2.0,1.134,6.5};
+        }
+
+        SECTION("zTransform with small signed doubles") {
+            DCBefore.vData = {-3.123,-6.0,-3.1,-5.1234,-7.3,-2.0,-1.134,-6.5};
+        }
+
+        SECTION("zTransform with small mixed-signed doubles") {
+            DCBefore.vData = {-3.123,6.0,-3.1,-5.1234,7.3,-2.0,-1.134,-6.5};
+        }
+
+        SECTION("zTransform with big unsigned doubles") {
+            DCBefore.vData = {23425234.234,7696858.22222,84863637.0,82828374.125,4738384.3};
+        }
+
+        SECTION("zTransform with big signed integers") {
+            DCBefore.vData = {-23425234.234,-7696858.22222,-84863637.0,-82828374.125,-4738384.3};
+        }
+
+        SECTION("zTransform with big mixed-signed integers") {
+            DCBefore.vData = {-23425234.234,-7696858.22222,84863637.0,-82828374.125,4738384.3};
+        }
+    }
+
+    DCAfter = DCBefore;
+
+    DCAfter.zTransform(true);
+    REQUIRE(DCAfter.vData.nearlyEqual(DCAfter.calcAverage(),0,TOLERANCE));
+    REQUIRE(DCAfter.vData.nearlyEqual(DCAfter.calcVariance(),1,TOLERANCE));
+    DCAfter.undoZTransform(DCBefore.calcAverage(),DCBefore.calcStdDeviation(),true);
+    REQUIRE(DCAfter.vData.nearlyEqual(DCBefore.calcAverage(),DCAfter.calcAverage(),TOLERANCE));
+    REQUIRE(DCAfter.vData.nearlyEqual(DCBefore.calcStdDeviation(),DCAfter.calcStdDeviation(),TOLERANCE));
+    for (int i = 0; i < DCAfter.vData.getSize(); i++) {
+        REQUIRE(DCBefore.vData.nearlyEqual(DCBefore.vData[i],DCAfter.vData[i],TOLERANCE));
+    }
+
 
 
 }
