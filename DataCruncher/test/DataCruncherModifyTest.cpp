dc5a887f819d4acf39183648ec96586feeb50458
anon <gitdev@web.de>
2018-01-04 20:48:20 +0100

    added testcases for z-transform

--- DataCruncher/test/DataCruncherModifyTest.cpp
+++ DataCruncher/test/DataCruncherModifyTest.cpp
@@ -16,8 +16,29 @@ TEST_CASE("Test DataCruncher.zTransform") {
     DataCruncher DC;
     SECTION("test") {
         DC.vData = {3,6,-3,5,7,2,1,6};
-        DC.zTransform(true);
-        DC.zTransform(false);
-
     }
+    SECTION("test") {
+        DC.vData = {6,6};
+    }
+
+        double averageBeforeZ  = DC.calcAverage();
+        double varianceBeforeZ = DC.calcVariance();
+        DC.zTransform(true);
+        double averageAfterZ  = DC.calcAverage();
+        double varianceAfterZ = DC.calcVariance();
+        REQUIRE(averageAfterZ == 0);
+        REQUIRE(varianceAfterZ == 1);
+        DC.undoZTransform(averageBeforeZ,varianceAfterZ,true);
+        REQUIRE(averageAfterZ == averageBeforeZ);
+        REQUIRE(varianceAfterZ == varianceBeforeZ);
+        REQUIRE(DC.vData[0] ==  3);
+        REQUIRE(DC.vData[0] ==  6);
+        REQUIRE(DC.vData[0] == -3);
+        REQUIRE(DC.vData[0] ==  5);
+        REQUIRE(DC.vData[0] ==  7);
+        REQUIRE(DC.vData[0] ==  2);
+        REQUIRE(DC.vData[0] ==  1);
+        REQUIRE(DC.vData[0] ==  6);
+
+
 }
